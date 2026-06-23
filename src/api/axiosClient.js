aa9ae85d23c94eabd2debec931383e820436ac3a
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2026-06-23 04:14:41 -0600

    Vercel config uploaded

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:5051/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  config.headers["X-API-KEY"] = "ADOPCION_API_2026";
+  config.headers["X-API-KEY"] = import.meta.env.VITE_API_KEY;
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
